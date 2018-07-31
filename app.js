dfbb48ede40ba85117a78680dfefa0bcac81d951
Riccardo Gabellini <gabelliniriccardo.94@gmail.com>
2018-07-31 14:28:41 +0200

    cleaned code

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json()); // support json encoded bodies
+
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
-app.use(function(req, res, next) {
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-autenticated = false;
 
 listOfUsers = [{
   'email': 'gabelliniriccardo.94@gmail.com',
@@ -18,85 +18,64 @@ listOfUsers = [{
   'name': 'Riccardo',
   'surname': 'Gabellini',
   'phone': '3333449042'
-},
-  {
-    'email': 'mariorossi@hotmail.com',
-    'password': 'mar10',
-    'name': 'Mario',
-    'surname': 'Rossi',
-    'phone': '3487963457'
-  },
-  {
-    'email': 'chiaramontinaro.94@gmail.com',
-    'password': 'ch1ar4',
-    'name': 'Chiara',
-    'surname': 'Montinaro',
-    'phone': '3392264061'
-  },
-  {
-    'email': 'gregoriorossi@hotmail.com',
-    'password': 'greg0r10',
-    'name': 'Gregorio',
-    'surname': 'Rossi',
-    'phone': '3484457678'
-  }];
-
+}];
 
 app.post('/login', function (req, res) {
 
-
-  autenticated = false;
-
-
-  emailTyped = req.body.email;
-  passwordTyped = req.body.password;
+  const emailTyped = req.body.email;
+  const passwordTyped = req.body.password;
 
   console.log('email : ' + emailTyped);
   console.log('password :' + passwordTyped);
 
-
-  let userFound = listOfUsers.find(user => user.email === emailTyped && user.password === passwordTyped);
-  autenticated = !!userFound;
-
+  const userFound = listOfUsers.find(user => user.email === emailTyped && user.password === passwordTyped);
+  const autenticated = !!userFound;
 
   if (autenticated) {
-    let {name, surname, phone, email} = userFound;
+    const {name, surname, phone, email} = userFound;
     res.json({name, surname, phone, email});
   } else {
     res.status('401').send({'error': 'The email or password is incorrect'});
   }
-
-
 });
 
+app.post('/newUser', function (req, res) {
+
+  const name = req.body.name;
+  const surname = req.body.surname;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log('new user sended: ', name, surname, phone, email, password);
+
+  let userFound = false;
+
+  listOfUsers.forEach((user) => {
+    if (user.email === email)
+      userFound = true;
+  });
+
+  if (!userFound) {
+    listOfUsers.push({email, password, name, surname, phone});
+    res.json('User added!')
+    console.log('RESULT : user added!')
+  } else {
+    res.status('401').send({'error': 'The email used already exists'});
+    console.log('RESULT: user refused. The email address already exists');
+  }
+});
 
 app.get('/users', function (req, res) {
-  if (autenticated) {
-    let listToSend = listOfUsers;
-    res.json(listToSend)
-  } else {
-    res.status('401').send({'error': 'The user is not authorized'});
-  }
-})
+  res.json(listOfUsers)
+});
 
 app.post('/users', function (req, res) {
-
-  if (autenticated) {
-
-    listOfUsers = req.body
-
-    res.json('Users Updated!')
-
-  }
-  else {
-    res.status('401').json('The user is not authorized');
-  }
-
-
+  listOfUsers = req.body
+  res.json('Users Updated!')
 });
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Users App is listening on port 3000!');
 });
-
